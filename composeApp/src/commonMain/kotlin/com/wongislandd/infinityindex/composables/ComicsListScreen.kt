688fc6889fd90fa99f49b6b97e2bd0d906f14314
Chris Wong <wongislandd@gmail.com>
2024-11-10 17:25:39 -0500

    a small amount of styling

--- composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/composables/ComicsListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/composables/ComicsListScreen.kt
@@ -1,18 +1,17 @@
 package com.wongislandd.infinityindex.composables
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -23,7 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.coil3.CoilImage
@@ -44,6 +43,7 @@ fun ComicsListScreen() {
     val screenState by viewModel.screenState.collectAsState()
     Box(
         modifier = Modifier
+            .background(Color.DarkGray)
             .fillMaxSize()
     ) {
         when (val state = screenState) {
@@ -72,47 +72,68 @@ fun ComicsListScreen() {
 private fun ComicsList(comicsList: List<NetworkComic>) {
     LazyVerticalGrid(columns = GridCells.Fixed(2)) {
         items(comicsList.size) { index ->
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                Column(
-                    modifier = Modifier.padding(16.dp),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    comicsList[index].thumbnail?.getFullUrl()?.also { url ->
-                        ComicImage(url)
-                    }
-                    Spacer(modifier = Modifier.size(8.dp))
-                    Text(
-                        comicsList[index].title ?: "No title",
-                        color = MaterialTheme.colors.onSurface, style = MaterialTheme.typography.h4,
-                        textAlign = TextAlign.Center
-                    )
-                }
+            ComicCard(comicsList[index])
+        }
+    }
+}
+
+@Composable
+private fun ComicCard(comic: NetworkComic, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+    ) {
+        Column {
+            // invalidate these, should never make it to UI if these are missing
+            comic.thumbnail?.getFullUrl()?.also { url ->
+                ComicImage(url, modifier = Modifier.height(300.dp))
+            }
+            comic.title?.also {
+                ComicTitlePlate(it)
             }
         }
     }
 }
 
 @Composable
-private fun ComicImage(url: String) {
-    CoilImage(
-        imageModel = { url },
-        imageOptions = ImageOptions(
-            contentScale = ContentScale.Inside,
-            alignment = Alignment.Center
-        ),
-        modifier = Modifier.fillMaxSize(),
-        component = rememberImageComponent {
-            ShimmerPlugin(
-                Shimmer.Flash(
-                    baseColor = Color.White,
-                    highlightColor = Color.LightGray,
-                    duration = 1000
+private fun ComicTitlePlate(title: String, modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.DarkGray)
+            .padding(8.dp)
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+private fun ComicImage(url: String, modifier: Modifier = Modifier) {
+    Box(modifier = Modifier.background(Color.DarkGray).fillMaxSize()) {
+        CoilImage(
+            imageModel = { url },
+            imageOptions = ImageOptions(
+                contentScale = ContentScale.Fit,
+                alignment = Alignment.Center
+            ),
+            modifier = modifier.align(Alignment.Center),
+            component = rememberImageComponent {
+                ShimmerPlugin(
+                    Shimmer.Flash(
+                        baseColor = Color.White,
+                        highlightColor = Color.LightGray,
+                        duration = 1000
+                    )
                 )
-            )
-        },
-        failure = {
-            Text("Could not load image.")
-        }
-    )
+            },
+            failure = {
+                Text("Could not load image.")
+            }
+        )
+    }
 }
