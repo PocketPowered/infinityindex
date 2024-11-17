ab0d65058f83f45f792df979fa48d3db0c44f2ff
Chris Wong <wongislandd@gmail.com>
2024-11-17 15:20:53 -0500

    Setting up see limited pagination and see all at the end

--- composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/infra/composables/EntityCard.kt
+++ composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/infra/composables/EntityCard.kt
@@ -1,10 +1,12 @@
 package com.wongislandd.infinityindex.infra.composables
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,8 +15,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -74,7 +79,34 @@ fun GhostEntityCard(modifier: Modifier = Modifier) {
 }
 
 @Composable
-private fun BaseEntityCard(modifier: Modifier = Modifier, content: @Composable ColumnScope.() -> Unit) {
+fun SeeMoreEntityCard(modifier: Modifier = Modifier) {
+    BaseEntityCard(modifier = modifier) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = "See all",
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onPrimary,
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Center
+            )
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                contentDescription = "See More",
+                tint = MaterialTheme.colors.onPrimary
+            )
+        }
+    }
+}
+
+@Composable
+private fun BaseEntityCard(
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
     Card(
         modifier = modifier
             .wrapContentHeight(),
