96b9297f9149ee193a4e111d3b7d5a5b2bd13716
Chris Wong <wongislandd@gmail.com>
2024-11-17 13:25:12 -0500

    Navigation solidifed

---
+++ composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/infra/navigation/NavigationHelper.kt
@@ -0,0 +1,115 @@
+package com.wongislandd.infinityindex.infra.navigation
+
+import ID_ARG
+import NavigationItem
+import ROOT_ENTITY_ARG
+import ROOT_ENTITY_ID_ARG
+import TITLE_ARG
+import com.wongislandd.infinityindex.infra.util.EntityType
+
+object NavigationHelper {
+
+    fun getRelatedListRoute(
+        rootEntityType: EntityType,
+        rootEntityId: Int,
+        relatedEntityType: EntityType,
+        title: String?
+    ): String {
+        val navigationItem = when (relatedEntityType) {
+            EntityType.CHARACTERS -> {
+                NavigationItem.RelatedComicListScreen
+            }
+
+            EntityType.COMICS -> {
+                NavigationItem.RelatedCharactersListScreen
+            }
+
+            EntityType.CREATORS -> {
+                NavigationItem.RelatedCreatorsListScreen
+            }
+
+            EntityType.EVENTS -> {
+                NavigationItem.RelatedEventsListScreen
+            }
+
+            EntityType.SERIES -> {
+                NavigationItem.RelatedSeriesListScreen
+            }
+
+            EntityType.STORIES -> {
+                NavigationItem.RelatedStoriesListScreen
+            }
+        }
+        val route = navigationItem.reconstructRoute(
+            mapOf(
+                ROOT_ENTITY_ARG to rootEntityType.name,
+                ROOT_ENTITY_ID_ARG to rootEntityId.toString(),
+                TITLE_ARG to title
+            )
+        )
+        return route
+    }
+
+    fun getAllListRoute(entityType: EntityType): String {
+        val navigationItem = when (entityType) {
+            EntityType.CHARACTERS -> {
+                NavigationItem.AllCharacterListScreen
+            }
+
+            EntityType.COMICS -> {
+                NavigationItem.AllComicListScreen
+            }
+
+            EntityType.CREATORS -> {
+                NavigationItem.AllCreatorListScreen
+            }
+
+            EntityType.EVENTS -> {
+                NavigationItem.AllEventListScreen
+            }
+
+            EntityType.SERIES -> {
+                NavigationItem.AllSeriesListScreen
+            }
+
+            EntityType.STORIES -> {
+                NavigationItem.AllStoriesListScreen
+            }
+        }
+        return navigationItem.route
+    }
+
+    fun getDetailsRoute(entityType: EntityType, id: Int): String {
+        val navigationItem = when (entityType) {
+            EntityType.CHARACTERS -> {
+                NavigationItem.CharacterDetailsScreen
+            }
+
+            EntityType.COMICS -> {
+                NavigationItem.ComicDetailsScreen
+            }
+
+            EntityType.CREATORS -> {
+                NavigationItem.CreatorDetailsScreen
+            }
+
+            EntityType.EVENTS -> {
+                NavigationItem.EventDetailsScreen
+            }
+
+            EntityType.SERIES -> {
+                NavigationItem.SeriesDetailsScreen
+            }
+
+            EntityType.STORIES -> {
+                NavigationItem.StoryDetailsScreen
+            }
+        }
+        val route = navigationItem.reconstructRoute(
+            mapOf(
+                ID_ARG to id
+            )
+        )
+        return route
+    }
+}
