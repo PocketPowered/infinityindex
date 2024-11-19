be13f364ec111969c2586ba829242d4a358c7621
Chris Wong <wongislandd@gmail.com>
2024-11-19 11:03:57 -0500

    Updating present text

--- composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/ComicConstants.kt
+++ composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/ComicConstants.kt
@@ -2,6 +2,6 @@ package com.wongislandd.infinityindex
 
 object ComicConstants {
     const val RELATED_DETAILS_MAX_ENTITY_RESULTS = 5
-    const val LIST_PAGE_SIZE = 20
+    const val LIST_PAGE_SIZE = 10
     const val SPLASH_SCREEN_DURATION = 2000L
 }
--- composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/entities/series/SeriesDetails.kt
+++ composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/entities/series/SeriesDetails.kt
@@ -2,13 +2,14 @@ package com.wongislandd.infinityindex.entities.series
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.wongislandd.infinityindex.infra.DateHelper
 import com.wongislandd.infinityindex.infra.EntityDetails
-import com.wongislandd.infinityindex.infra.composables.RelatedCreatorsSection
-import com.wongislandd.infinityindex.infra.composables.InformationSnippet
-import com.wongislandd.infinityindex.models.local.Series
 import com.wongislandd.infinityindex.infra.composables.DetailsSection
+import com.wongislandd.infinityindex.infra.composables.InformationSnippet
+import com.wongislandd.infinityindex.infra.composables.RelatedCreatorsSection
 import com.wongislandd.infinityindex.infra.composables.SimpleDetailsSection
 import com.wongislandd.infinityindex.infra.util.safeLet
+import com.wongislandd.infinityindex.models.local.Series
 
 @Composable
 fun SeriesDetails(series: Series, modifier: Modifier = Modifier) {
@@ -19,7 +20,9 @@ fun SeriesDetails(series: Series, modifier: Modifier = Modifier) {
                 InformationSnippet("Rating", rating)
             }
             safeLet(series.startYear, series.endYear) { startYear, endYear ->
-                InformationSnippet("Active", "$startYear - $endYear")
+                val endYearOrPresent =
+                    if (DateHelper.isYearInTheFuture(endYear)) "Present" else endYear
+                InformationSnippet("Active", "$startYear - $endYearOrPresent")
             }
         }
         RelatedCreatorsSection(series.creatorsByRole, emptyMap())
