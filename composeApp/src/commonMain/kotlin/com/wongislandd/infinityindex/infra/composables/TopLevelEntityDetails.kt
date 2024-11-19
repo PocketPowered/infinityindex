9bf60696c61bcf979ae5d658ea54fe10dcbd5fdf
Chris Wong <wongislandd@gmail.com>
2024-11-19 09:41:21 -0500

    Adding some rounding to the general icons

--- composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/infra/composables/TopLevelEntityDetails.kt
+++ composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/infra/composables/TopLevelEntityDetails.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -32,11 +34,13 @@ fun TopLevelEntityDetails(entity: EntityModel, modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        MarvelImage(
-            image = entity.image,
-            tint = MaterialTheme.colors.onSurface,
-            modifier = Modifier.fillMaxWidth()
-        )
+        Card(modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(8.dp)) {
+            MarvelImage(
+                image = entity.image,
+                tint = MaterialTheme.colors.onSurface,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
         Text(
             text = entity.displayName,
             modifier = Modifier.padding(8.dp),
