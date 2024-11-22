a63c650a08c4967f58c05d60af764bfbd62a02ff
Chris Wong <wongislandd@gmail.com>
2024-11-21 20:03:35 -0500

    update version num

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -141,8 +141,8 @@ android {
         applicationId = "com.wongislandd.infinityindex"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.4"
     }
     packaging {
         resources {
