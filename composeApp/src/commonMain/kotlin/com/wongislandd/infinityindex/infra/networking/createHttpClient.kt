1d7be83b0ba7b86a2a797279581611ead02771a6
Chris Wong <wongislandd@gmail.com>
2025-04-14 12:37:55 -0400

    downgrading ktor to 3.0.1 to avoid content length validation

--- composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/infra/networking/createHttpClient.kt
+++ composeApp/src/commonMain/kotlin/com/wongislandd/infinityindex/infra/networking/createHttpClient.kt
@@ -11,6 +11,8 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import okio.ByteString.Companion.encodeUtf8
